beba58f5d2a5bafb2eb4f79d9d91df0a7b91a2e3
alyadel-moh <alyadel1555@gmail.com>
2025-07-24 12:31:39 +0300

    get optimized images

---
+++ src/image-url.ts
@@ -0,0 +1,6 @@
+const getCroppedImageUrl = (url : string) =>{
+  const index = url.indexOf('media/') + 'media/'.length  // we added length to add crop after media
+  return url.slice(0,index) + "crop/600/400/" + url.slice(index) //index is staring pos of media prameter  0 mean from the begining
+}
+export default getCroppedImageUrl
+// download smaller imahe to make loading faster
