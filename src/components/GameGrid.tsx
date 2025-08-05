66cbede991e30521d5ecd12d60538b639b3ba690
alyadel-moh <alyadel1555@gmail.com>
2025-08-05 17:11:31 +0300

    refactor gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,30 +10,25 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6]; //array with six items each item rendered to skeleton comp
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      {/* add to x to /games to make error */}
-      {/* we columns count acc to screen size */}
-      {/* padding to not be too close to screen edgess */}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((Skeleton) => (
-            <GameCardCountainer key={Skeleton}>
-              <GameCardSkeleton />
-            </GameCardCountainer>
-          ))}
-        {data.map((game) => (
-          <GameCardCountainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((Skeleton) => (
+          <GameCardCountainer key={Skeleton}>
+            <GameCardSkeleton />
           </GameCardCountainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardCountainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardCountainer>
+      ))}
+    </SimpleGrid>
   );
 };
 // comp doesn't know about making any HTTP request
