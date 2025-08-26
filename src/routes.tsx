b27a217fad4d2dce66166b30ecaa8067cf625691
alyadel-moh <alyadel1555@gmail.com>
2025-08-26 17:35:09 +0300

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
 import GameDetailPage from "./pages/GameDetailPage";
+import Errorpage from "./pages/Errorpage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <Errorpage />,
     children: [
       {
         index: true,
